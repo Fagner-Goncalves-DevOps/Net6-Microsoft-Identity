9493baf264c2e7f05640feb2f38cd4c8c551bd86
FagnerGoncalves <FagnerGoncalves@EC2AMAZ-KD81SO0>
2022-11-30 20:43:25 +0000

    Net6-MVC-Auth-Roles-Claims

---
+++ NetIdentityModel/Models/AccountDto/UserRoleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NetIdentityModel.Models.AccountDto
+{
+    public class UserRoleDto
+    {
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
